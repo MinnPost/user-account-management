576fdff419f50a67bd1f818344e610641d30568c
Jonathan Stegall <jstegall@minnpost.com>
2018-01-11 15:01:59 -0600

    reorder methods so it's not so hard to find them

--- user-account-management.php
+++ user-account-management.php
@@ -43,21 +43,6 @@ class User_Account_Management {
 	}
 
 	/**
-	 * New user registrations should have display_name set
-	 * to 'firstname lastname'.
-	 *
-	 * @param string $name
-	 */
-	public function set_default_display_name( $name ) {
-		$first_name = isset( $_POST['first_name'] ) ? sanitize_text_field( $_POST['first_name'] ) : '';
-		$last_name = isset( $_POST['last_name'] ) ? sanitize_text_field( $_POST['last_name'] ) : '';
-		if ( '' !== $first_name && '' !== $last_name ) {
-			$name = $first_name . ' ' . $last_name;
-		}
-		return $name;
-	}
-
-	/**
 	 * Plugin activation hook.
 	 *
 	 * Creates all WordPress pages needed by the plugin.
@@ -236,195 +221,6 @@ class User_Account_Management {
 	}
 
 	/**
-	 * Renders the contents of the given template to a string and returns it.
-	 *
-	 * @param string $template_name The name of the template to render (without .php)
-	 * @param string $location      Folder location for the template (ie front-end or admin)
-	 * @param array  $attributes    The PHP variables for the template
-	 *
-	 * @return string               The contents of the template.
-	 */
-	private function get_template_html( $template_name, $location = '', $attributes = null ) {
-		if ( ! $attributes ) {
-			$attributes = array();
-		}
-
-		if ( '' !== $location ) {
-			$location = $location . '/';
-		}
-
-		ob_start();
-
-		do_action( 'user_account_management_before_' . $template_name );
-
-		// allow users to put templates into their theme
-		if ( file_exists( get_theme_file_path() . '/' . $this->slug . '-templates/' . $template_name . '.php' ) ) {
-			$file = get_theme_file_path() . '/' . $this->slug . '-templates/' . $template_name . '.php';
-		} else {
-			$file = plugin_dir_path( __FILE__ ) . 'templates/' . $location . $template_name . '.php';
-		}
-
-		require( $file );
-
-		do_action( 'user_account_management_after_' . $template_name );
-
-		$html = ob_get_contents();
-		ob_end_clean();
-
-		return $html;
-	}
-
-	/**
-	 * Redirect the user to the custom login page instead of wp-login.php.
-	 */
-	function redirect_to_custom_login() {
-		if ( 'GET' === $_SERVER['REQUEST_METHOD'] ) {
-			$redirect_to = isset( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : null;
-			if ( is_user_logged_in() ) {
-				$this->redirect_logged_in_user( $redirect_to );
-				exit;
-			}
-
-			// The rest are redirected to the login page
-			$login_url = site_url( 'user/login' );
-			if ( ! empty( $redirect_to ) ) {
-				$login_url = add_query_arg( 'redirect_to', $redirect_to, $login_url );
-			}
-
-			wp_redirect( $login_url );
-			exit;
-		}
-	}
-
-	/**
-	 * Redirects the user to the correct page depending on whether he / she
-	 * is an admin or not.
-	 *
-	 * @param string $redirect_to   An optional redirect_to URL for admin users
-	 */
-	private function redirect_logged_in_user( $redirect_to = null ) {
-		$user = wp_get_current_user();
-		if ( in_array( $user->roles[0], array( 'administrator' ) ) ) {
-			if ( $redirect_to ) {
-				wp_safe_redirect( $redirect_to );
-			} else {
-				wp_redirect( admin_url() );
-			}
-		} else {
-			if ( $redirect_to ) {
-				wp_safe_redirect( $redirect_to );
-			} else {
-				wp_redirect( site_url( 'user' ) );
-			}
-		}
-	}
-
-	/**
-	 * Redirect the user after authentication if there were any errors.
-	 *
-	 * @param Wp_User|Wp_Error  $user       The signed in user, or the errors that have occurred during login.
-	 * @param string            $username   The user name used to log in.
-	 * @param string            $password   The password used to log in.
-	 *
-	 * @return Wp_User|Wp_Error The logged in user, or error information if there were errors.
-	 */
-	function maybe_redirect_at_authenticate( $user, $username, $password ) {
-		// Check if the earlier authenticate filter (most likely,
-		// the default WordPress authentication) functions have found errors
-		if ( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
-			if ( is_wp_error( $user ) ) {
-				$error_codes = join( ',', $user->get_error_codes() );
-				$login_url = site_url( 'user/login' );
-				$login_url = add_query_arg( 'login', $error_codes, $login_url );
-				wp_redirect( $login_url );
-				exit;
-			}
-		}
-		return $user;
-	}
-
-	/**
-	 * Finds and returns a matching error message for the given error code.
-	 *
-	 * @param string $error_code    The error code to look up.
-	 *
-	 * @return string               An error message.
-	 */
-	private function get_error_message( $error_code ) {
-		switch ( $error_code ) {
-			case 'empty_username':
-				return __( 'You did not enter an email address.', 'user-account-management' );
-			case 'empty_password':
-				return __( 'You did not enter a password.', 'user-account-management' );
-			case 'invalid_username':
-			case 'invalid_email':
-				return __(
-					"We couldn't find an account with that email address. Maybe you used a different one when signing up?",
-					'user-account-management'
-				);
-			case 'incorrect_password':
-				$err = __(
-					"The password you entered wasn't right. We can help you <a href='%s'>reset your password</a>.",
-					'user-account-management'
-				);
-				return sprintf( $err, wp_lostpassword_url() );
-			case 'email':
-				return __( 'The email address you entered is not valid.', 'user-account-management' );
-			case 'email_exists':
-				return __( 'An account already exists with this email address. Is it yours?', 'user-account-management' );
-			case 'closed':
-				return __( 'Registering new users is currently not allowed.', 'user-account-management' );
-			default:
-				break;
-		}
-		return __( 'An unknown error occurred. Please try again later.', 'user-account-management' );
-	}
-
-	/**
-	 * Redirect to custom login page after the user has been logged out.
-	 */
-	public function redirect_after_logout() {
-		$redirect_url = site_url( 'user/login/?logged_out=true' );
-		wp_safe_redirect( $redirect_url );
-		exit;
-	}
-
-	/**
-	 * Returns the URL to which the user should be redirected after the (successful) login.
-	 *
-	 * @param string           $redirect_to           The redirect destination URL.
-	 * @param string           $requested_redirect_to The requested redirect destination URL passed as a parameter.
-	 * @param WP_User|WP_Error $user                  WP_User object if login was successful, WP_Error object otherwise.
-	 *
-	 * @return string Redirect URL
-	 */
-	public function redirect_after_login( $redirect_to, $requested_redirect_to, $user ) {
-		$redirect_url = site_url();
-
-		if ( ! isset( $user->ID ) ) {
-			return $redirect_url;
-		}
-
-		if ( in_array( $user->roles[0], array( 'administrator' ) ) ) {
-			// Use the redirect_to parameter if one is set, otherwise redirect to admin dashboard.
-			if ( '' === $requested_redirect_to ) {
-				$redirect_url = admin_url();
-			} else {
-				$redirect_url = $requested_redirect_to;
-			}
-		} else {
-			// Non-admin users go to their account page after login, unless another url is supplied
-			if ( '' === $requested_redirect_to ) {
-				$redirect_url = site_url( 'user' );
-			} else {
-				$redirect_url = $requested_redirect_to;
-			}
-		}
-
-		return wp_validate_redirect( $redirect_url, site_url() );
-	}
-
-	/**
 	 * A shortcode for rendering the new user registration form.
 	 *
 	 * @param  array   $attributes  Shortcode attributes.
@@ -502,6 +298,37 @@ class User_Account_Management {
 	}
 
 	/**
+	 * Redirect the user to the custom login page instead of wp-login.php.
+	 */
+	public function redirect_to_custom_login() {
+		if ( 'GET' === $_SERVER['REQUEST_METHOD'] ) {
+			$redirect_to = isset( $_REQUEST['redirect_to'] ) ? $_REQUEST['redirect_to'] : null;
+			if ( is_user_logged_in() ) {
+				$this->redirect_logged_in_user( $redirect_to );
+				exit;
+			}
+
+			// The rest are redirected to the login page
+			$login_url = site_url( 'user/login' );
+			if ( ! empty( $redirect_to ) ) {
+				$login_url = add_query_arg( 'redirect_to', $redirect_to, $login_url );
+			}
+
+			wp_redirect( $login_url );
+			exit;
+		}
+	}
+
+	/**
+	 * Redirect to custom login page after the user has been logged out.
+	 */
+	public function redirect_after_logout() {
+		$redirect_url = site_url( 'user/login/?logged_out=true' );
+		wp_safe_redirect( $redirect_url );
+		exit;
+	}
+
+	/**
 	 * Redirects the user to the custom registration page instead
 	 * of wp-login.php?action=register.
 	 */
@@ -517,65 +344,13 @@ class User_Account_Management {
 	}
 
 	/**
-	 * Validates and then completes the new user signup process if all went well.
-	 *
-	 * @param string $email         The new user's email address
-	 * @param string $password      The new user's password
-	 * @param string $first_name    The new user's first name
-	 * @param string $last_name     The new user's last name
-	 * @param string $zip_code      The new user's zip code
-	 * @param string $country       The new user's country
-	 *
-	 * @return int|WP_Error         The id of the user that was created, or error if failed.
-	 */
-	private function register_user( $email, $password, $first_name, $last_name, $zip_code, $country = '' ) {
-		$errors = new WP_Error();
-
-		// Email address is used as both username and email. It is also the only
-		// parameter we need to validate
-		if ( ! is_email( $email ) ) {
-			$errors->add( 'email', $this->get_error_message( 'email' ) );
-			return $errors;
-		}
-
-		if ( username_exists( $email ) || email_exists( $email ) ) {
-			$errors->add( 'email_exists', $this->get_error_message( 'email_exists' ) );
-			return $errors;
-		}
-
-		// Generate the password so that the subscriber will have to check email...
-		//$password = wp_generate_password( 12, false );
-
-		$user_data = array(
-			'user_login'    => $email,
-			'user_email'    => $email,
-			'user_pass'     => $password,
-			'first_name'    => $first_name,
-			'last_name'     => $last_name,
-			'display_name'  => $first_name . ' ' . $last_name,
-			'nickname'      => $first_name,
-		);
-
-		$user_id = wp_insert_user( $user_data );
-
-		if ( '' !== $zip_code ) {
-			update_user_meta( $user_id, '_zip_code', $zip_code );
-		}
-
-		if ( '' !== $country ) {
-			update_user_meta( $user_id, '_country', $country );
-		}
-
-		//wp_new_user_notification( $user_id, $password );
-
-		return $user_id;
-	}
-
-	/**
 	 * Handles the registration of a new user.
 	 *
 	 * Used through the action hook "login_form_register" activated on wp-login.php
 	 * when accessed through the registration action.
+	 *
+	 * This method logs in the user if they are successfully registered, and then it redirects them.
+	 *
 	 */
 	public function do_register_user() {
 		if ( 'POST' == $_SERVER['REQUEST_METHOD'] ) {
@@ -626,6 +401,74 @@ class User_Account_Management {
 		}
 	}
 
+	/**
+	 * Redirect the user after authentication if there were any errors.
+	 *
+	 * @param Wp_User|Wp_Error  $user       The signed in user, or the errors that have occurred during login.
+	 * @param string            $username   The user name used to log in.
+	 * @param string            $password   The password used to log in.
+	 *
+	 * @return Wp_User|Wp_Error The logged in user, or error information if there were errors.
+	 */
+	public function maybe_redirect_at_authenticate( $user, $username, $password ) {
+		// Check if the earlier authenticate filter (most likely,
+		// the default WordPress authentication) functions have found errors
+		if ( 'POST' === $_SERVER['REQUEST_METHOD'] ) {
+			if ( is_wp_error( $user ) ) {
+				$error_codes = join( ',', $user->get_error_codes() );
+				$login_url = site_url( 'user/login' );
+				$login_url = add_query_arg( 'login', $error_codes, $login_url );
+				wp_redirect( $login_url );
+				exit;
+			}
+		}
+		return $user;
+	}
+
+	/**
+	 * Returns the URL to which the user should be redirected after the (successful) login.
+	 *
+	 * @param string           $redirect_to           The redirect destination URL.
+	 * @param string           $requested_redirect_to The requested redirect destination URL passed as a parameter.
+	 * @param WP_User|WP_Error $user                  WP_User object if login was successful, WP_Error object otherwise.
+	 *
+	 * @return string Redirect URL
+	 */
+	public function redirect_after_login( $redirect_to, $requested_redirect_to, $user ) {
+		$redirect_url = site_url();
+
+		if ( ! isset( $user->ID ) ) {
+			return $redirect_url;
+		}
+
+		if ( in_array( $user->roles[0], array( 'administrator' ) ) ) {
+			// Use the redirect_to parameter if one is set, otherwise redirect to admin dashboard.
+			if ( '' === $requested_redirect_to ) {
+				$redirect_url = admin_url();
+			} else {
+				$redirect_url = $requested_redirect_to;
+			}
+		} else {
+			// Non-admin users go to their account page after login, unless another url is supplied
+			if ( '' === $requested_redirect_to ) {
+				$redirect_url = site_url( 'user' );
+			} else {
+				$redirect_url = $requested_redirect_to;
+			}
+		}
+
+		return wp_validate_redirect( $redirect_url, site_url() );
+	}
+
+	/**
+	 * Allow users to use email addresses as username, preserving special characters
+	 *
+	 * @param string           $username           The username that will be stored
+	 * @param string           $raw_username       The posted username value
+	 * @param bool             $strict             Whether to be strict about special chars
+	 *
+	 * @return string $username
+	 */
 	public function allow_email_as_username( $username, $raw_username, $strict ) {
 		// This avoids infinite looping! We only re-run if strict was set.
 		if ( $strict && false !== is_email( $username ) ) {
@@ -633,6 +476,173 @@ class User_Account_Management {
 		} else {
 			return $username;
 		}
+	}
+
+	/**
+	 * New user registrations should have display_name set
+	 * to 'firstname lastname'.
+	 *
+	 * @param string $name
+	 *
+	 * @return string name
+	 *
+	 */
+	public function set_default_display_name( $name ) {
+		$first_name = isset( $_POST['first_name'] ) ? sanitize_text_field( $_POST['first_name'] ) : '';
+		$last_name = isset( $_POST['last_name'] ) ? sanitize_text_field( $_POST['last_name'] ) : '';
+		if ( '' !== $first_name && '' !== $last_name ) {
+			$name = $first_name . ' ' . $last_name;
+		}
+		return $name;
+	}
+
+	/**
+	 * Redirects the user to the correct page depending on whether he / she
+	 * is an admin or not.
+	 *
+	 * @param string $redirect_to   An optional redirect_to URL for admin users
+	 */
+	private function redirect_logged_in_user( $redirect_to = null ) {
+		$user = wp_get_current_user();
+		if ( in_array( $user->roles[0], array( 'administrator' ) ) ) {
+			if ( $redirect_to ) {
+				wp_safe_redirect( $redirect_to );
+			} else {
+				wp_redirect( admin_url() );
+			}
+		} else {
+			if ( $redirect_to ) {
+				wp_safe_redirect( $redirect_to );
+			} else {
+				wp_redirect( site_url( 'user' ) );
+			}
+		}
+	}
+
+	/**
+	 * Validates and then completes the new user signup process if all went well.
+	 *
+	 * @param string $email         The new user's email address
+	 * @param string $password      The new user's password
+	 * @param string $first_name    The new user's first name
+	 * @param string $last_name     The new user's last name
+	 * @param string $zip_code      The new user's zip code
+	 * @param string $country       The new user's country
+	 *
+	 * @return int|WP_Error         The id of the user that was created, or error if failed.
+	 */
+	private function register_user( $email, $password, $first_name, $last_name, $zip_code, $country = '' ) {
+		$errors = new WP_Error();
+
+		// Email address is used as both username and email. It is also the only
+		// parameter we need to validate
+		if ( ! is_email( $email ) ) {
+			$errors->add( 'email', $this->get_error_message( 'email' ) );
+			return $errors;
+		}
+
+		if ( username_exists( $email ) || email_exists( $email ) ) {
+			$errors->add( 'email_exists', $this->get_error_message( 'email_exists' ) );
+			return $errors;
+		}
+
+		$user_data = array(
+			'user_login'    => $email,
+			'user_email'    => $email,
+			'user_pass'     => $password,
+			'first_name'    => $first_name,
+			'last_name'     => $last_name,
+			'display_name'  => $first_name . ' ' . $last_name,
+			'nickname'      => $first_name,
+		);
+
+		$user_id = wp_insert_user( $user_data );
+
+		if ( '' !== $zip_code ) {
+			update_user_meta( $user_id, '_zip_code', $zip_code );
+		}
+
+		if ( '' !== $country ) {
+			update_user_meta( $user_id, '_country', $country );
+		}
+
+		return $user_id;
+	}
+
+	/**
+	 * Renders the contents of the given template to a string and returns it.
+	 *
+	 * @param string $template_name The name of the template to render (without .php)
+	 * @param string $location      Folder location for the template (ie front-end or admin)
+	 * @param array  $attributes    The PHP variables for the template
+	 *
+	 * @return string               The contents of the template.
+	 */
+	private function get_template_html( $template_name, $location = '', $attributes = null ) {
+		if ( ! $attributes ) {
+			$attributes = array();
+		}
+
+		if ( '' !== $location ) {
+			$location = $location . '/';
+		}
+
+		ob_start();
+
+		do_action( 'user_account_management_before_' . $template_name );
+
+		// allow users to put templates into their theme
+		if ( file_exists( get_theme_file_path() . '/' . $this->slug . '-templates/' . $template_name . '.php' ) ) {
+			$file = get_theme_file_path() . '/' . $this->slug . '-templates/' . $template_name . '.php';
+		} else {
+			$file = plugin_dir_path( __FILE__ ) . 'templates/' . $location . $template_name . '.php';
+		}
+
+		require( $file );
+
+		do_action( 'user_account_management_after_' . $template_name );
+
+		$html = ob_get_contents();
+		ob_end_clean();
+
+		return $html;
+	}
+
+	/**
+	 * Finds and returns a matching error message for the given error code.
+	 *
+	 * @param string $error_code    The error code to look up.
+	 *
+	 * @return string               An error message.
+	 */
+	private function get_error_message( $error_code ) {
+		switch ( $error_code ) {
+			case 'empty_username':
+				return __( 'You did not enter an email address.', 'user-account-management' );
+			case 'empty_password':
+				return __( 'You did not enter a password.', 'user-account-management' );
+			case 'invalid_username':
+			case 'invalid_email':
+				return __(
+					"We couldn't find an account with that email address. Maybe you used a different one when signing up?",
+					'user-account-management'
+				);
+			case 'incorrect_password':
+				$err = __(
+					"The password you entered wasn't right. We can help you <a href='%s'>reset your password</a>.",
+					'user-account-management'
+				);
+				return sprintf( $err, wp_lostpassword_url() );
+			case 'email':
+				return __( 'The email address you entered is not valid.', 'user-account-management' );
+			case 'email_exists':
+				return __( 'An account already exists with this email address. Is it yours?', 'user-account-management' );
+			case 'closed':
+				return __( 'Registering new users is currently not allowed.', 'user-account-management' );
+			default:
+				break;
+		}
+		return __( 'An unknown error occurred. Please try again later.', 'user-account-management' );
 	}
 
 }
