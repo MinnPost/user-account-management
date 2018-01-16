2d78bdfe9962d03821281f95cb8c73ea9d388c3a
Jonathan Stegall <jstegall@minnpost.com>
2018-01-16 10:33:44 -0600

    caching

--- user-account-management.php
+++ user-account-management.php
@@ -17,9 +17,9 @@ class User_Account_Management {
 	 */
 	public function __construct() {
 
-		$this->slug = 'user-account-management';
-
+		$this->option_prefix = 'user_account_management';
 		$this->version = '0.0.1';
+		$this->slug = 'user-account-management';
 		$this->add_actions();
 
 	}
@@ -55,6 +55,11 @@ class User_Account_Management {
 		add_filter( 'sanitize_user', array( $this, 'allow_email_as_username' ), 10, 3 ); // register
 		add_filter( 'pre_user_display_name', array( $this, 'set_default_display_name' ) ); // register
 		add_filter( 'retrieve_password_message', array( $this, 'replace_retrieve_password_message' ), 10, 4 ); // lost password
+		$this->cache = true;
+		if ( true === $this->cache ) {
+			$this->acct_transients = new User_Account_Management_Transient( 'user_account_transients' );
+		}
+
 	}
 
 	/**
@@ -895,6 +900,31 @@ class User_Account_Management {
 		return $html;
 	}
 
+
+	/**
+	 * Check to see if this API call exists in the cache
+	 * if it does, return the transient for that key
+	 *
+	 * @param mixed $call The API call we'd like to make.
+	 * @return $this->form_transients->get $cachekey
+	 */
+	private function cache_get( $call ) {
+		$cachekey = md5( wp_json_encode( $call ) );
+		return $this->acct_transients->get( $cachekey );
+	}
+
+	/**
+	 * Create a cache entry for the current result, with the url and args as the key
+	 *
+	 * @param mixed $call The API query name.
+	 * @return Bool whether or not the value was set
+	 * @link https://wordpress.stackexchange.com/questions/174330/transient-storage-location-database-xcache-w3total-cache
+	 */
+	private function cache_set( $call, $data ) {
+		$cachekey = md5( wp_json_encode( $call ) );
+		return $this->acct_transients->set( $cachekey, $data );
+	}
+
 	/**
 	 * Finds and returns a matching error message for the given error code.
 	 *
@@ -942,6 +972,97 @@ class User_Account_Management {
 	}
 
 }
+
+/**
+ * Class to store all theme/plugin transients as an array in one WordPress transient
+ **/
+class User_Account_Management_Transient {
+
+	protected $name;
+
+	public $cache_expiration;
+
+	/**
+	 * Constructor which sets cache options and the name of the field that lists this plugin's cache keys.
+	 *
+	 * @param string $name The name of the field that lists all cache keys.
+	 */
+	public function __construct( $name ) {
+		$this->name = $name;
+		$this->cache_expiration = 2592000; // cache it for a month
+		$this->cache_prefix = esc_sql( 'acct_mgmt_' );
+	}
+
+	/**
+	 * Get the transient that lists all the other transients for this plugin.
+	 *
+	 * @return mixed value of transient. False of empty, otherwise array.
+	 */
+	public function all_keys() {
+		return get_transient( $this->name );
+	}
+
+	/**
+	 * Set individual transient, and add its key to the list of this plugin's transients.
+	 *
+	 * @param string $cachekey the key for this cache item
+	 * @param mixed $value the value of the cache item
+	 * @param int $cache_expiration. How long the plugin key cache, and this individual item cache, should last before expiring.
+	 * @return mixed value of transient. False of empty, otherwise array.
+	 */
+	public function set( $cachekey, $value ) {
+
+		$prefix = $this->cache_prefix;
+		$cachekey = $prefix . $cachekey;
+
+		$keys = $this->all_keys();
+		$keys[] = $cachekey;
+		set_transient( $this->name, $keys, $this->cache_expiration );
+
+		return set_transient( $cachekey, $value, $this->cache_expiration );
+	}
+
+	/**
+	 * Get the individual cache value
+	 *
+	 * @param string $cachekey the key for this cache item
+	 * @return mixed value of transient. False of empty, otherwise array.
+	 */
+	public function get( $cachekey ) {
+		$prefix = $this->cache_prefix;
+		$cachekey = $prefix . $cachekey;
+		return get_transient( $cachekey );
+	}
+
+	/**
+	 * Delete the individual cache value
+	 *
+	 * @param string $cachekey the key for this cache item
+	 * @return bool True if successful, false otherwise.
+	 */
+	public function delete( $cachekey ) {
+		$prefix = $this->cache_prefix;
+		$cachekey = $prefix . $cachekey;
+		return delete_transient( $cachekey );
+	}
+
+	/**
+	 * Delete the entire cache for this plugin
+	 *
+	 * @return bool True if successful, false otherwise.
+	 */
+	public function flush() {
+		$keys = $this->all_keys();
+		$result = true;
+		foreach ( $keys as $key ) {
+			$result = delete_transient( $key );
+		}
+		$result = delete_transient( $this->name );
+		return $result;
+	}
+
+}
+
 
 register_activation_hook( __FILE__, array( 'User_Account_Management', 'plugin_activated' ) );
 
